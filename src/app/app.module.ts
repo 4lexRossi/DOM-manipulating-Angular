dbcb15ba52c7f64f876b271a0074316be79264e9
4lexRossi <devalexrossi@gmail.com>
2021-04-25 21:05:05 -0300

    for directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlosIfDirective } from './alos-if.directive';
+import { alosForDirective } from './alos-for.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlosIfDirective
+    AlosIfDirective,
+    alosForDirective
   ],
   imports: [
     BrowserModule,
